836ea7f6cb03a893933e906051068fc1b8c4ff90
Lehi Qr <lehi_10@hotmail.com>
2017-11-27 22:34:24 -0500

    Client Profile
    Updating

--- iSeller/apps/cliente/urls.py
+++ iSeller/apps/cliente/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from apps.cliente.views import index
+from apps.cliente.views import index,cliente_list
 
 urlpatterns =[ 
     url(r'^$',index, name='index'),
+    url(r'^listar$',cliente_list,name='cliente_listar'),
 ]
--- iSeller/apps/cliente/views.py
+++ iSeller/apps/cliente/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
-
-
-
-from apps.cliente.models  import Cliente
-from apps.registro.models  import Persona
-
+from apps.registro.models import Persona
 # Create your views here.
-
-
 
 def index(request):
     return render(request, 'cliente/index.html')
-
-  
-
-
-    
+def cliente_list(request):
+	print ("prueba p papi")
+	cliente_ = Persona.objects.all()
+	contexto= {'mi_cliente':cliente_}
+	print(contexto)
+	return render(request,'cliente/cliente_list.html',contexto)
