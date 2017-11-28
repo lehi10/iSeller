2ee190ac0029b1a3b7168ebdcd05691d38b3bcd4
Lehi Qr <lehi_10@hotmail.com>
2017-11-27 20:41:31 -0500

    Commit sin importancia

--- iSeller/apps/cliente/views.py
+++ iSeller/apps/cliente/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+
+
+from apps.cliente.models  import Cliente
+from apps.registro.models  import Persona
+
 # Create your views here.
+
+
 
 def index(request):
     return render(request, 'cliente/index.html')
 
+  
+
+
+    
