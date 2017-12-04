816e5684d7007ac6397d2d2d78ca5eb7c9eb7148
Lena <karengvcs@gmail.com>
2017-12-03 21:46:12 -0500

    Validations of registers and login

--- iSeller/apps/registro/forms.py
+++ iSeller/apps/registro/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from apps.registro.models import Persona
+import time
+from django.utils.translation import gettext as _
+from django.core.validators import validate_email
+
 
 
 class RegistroForm(forms.ModelForm):
@@ -42,13 +46,68 @@ class RegistroForm(forms.ModelForm):
 			'fecha_nac':forms.DateInput(attrs={'class': 'form-control','type':'date'}),
 			'id_doc':forms.TextInput(attrs={'class': 'form-control'}),
 			'permisos':forms.TextInput(attrs={'class': 'form-control'}),
-			'email':forms.TextInput(attrs={'class': 'form-control'}),
+			'email':forms.EmailInput(attrs={'class': 'form-control'}),
 			'telf':forms.TextInput(attrs={'class': 'form-control'}),
 			'domicilio':forms.TextInput(attrs={'class': 'form-control'}),
 		}
 
+	def clean_usuario(self):
+		usuario = self.cleaned_data.get("usuario")
+		if  Persona.objects.filter(usuario=usuario).exists():
+			raise forms.ValidationError("El nombre de usuario ya esta en uso")
+		return usuario
+
+	def clean_password(self):
+		password = self.cleaned_data.get("password")
+		if not len(password) >= 8:
+			raise forms.ValidationError("La contraseña debe tener como mínimo 8 caracteres")
+		return password
+
+	def clean_fecha_nac(self):
+		fecha_nac = self.cleaned_data.get("fecha_nac")
+		if not int(time.strftime("%Y")) - int(fecha_nac.year) >= 18:
+			raise forms.ValidationError("Usted no cumple con el límite  mínimo de edad")
+		return fecha_nac
+
+	def clean_id_doc(self):
+		id_doc = self.cleaned_data.get("id_doc")
+		if not len(id_doc) == 8:
+			raise forms.ValidationError("Su ID debe ser de 8 dígitos")
+		if id_doc.isdigit() == False:
+			raise forms.ValidationError("Su ID no es válido")
+		return id_doc
+
+	def clean_email(self):
+		email = self.cleaned_data.get("email")
+		email_base, proveedor = email.split("@")
+		if -1 != proveedor.find("."):
+			dominio, extension = proveedor.split(".")
+			if not extension == "com":
+				raise forms.ValidationError("Utilize un email con la extension .com")
+			if Persona.objects.filter(email=email).exists():
+				raise forms.ValidationError("Este correo ya esta siendo usado")
+		else :
+			raise forms.ValidationError("Utilize un email válido")
+		#validate_email(email)
+		return email
+
+	def clean_telf(self):
+		telf = self.cleaned_data.get("telf")
+		if len(telf) >= 6:
+			if telf.isdigit() == False:
+				raise forms.ValidationError("Ingrese un número válido")
+		else: 
+			raise forms.ValidationError("Ingrese un número válido")
+		return telf
+
+
 class LoginForm(forms.Form):
 	username = forms.CharField( widget=forms.TextInput,required=True)
 	password = forms.CharField(	widget=forms.TextInput, required=True)
+	"""def clean_username(self):
+		username = self.cleaned_data.get("username")
+		if Persona.objects.filter(usuario=username).exists():
+
+		elif Persona.objects.filter(email=username).exists():"""
 
 
--- iSeller/apps/registro/views.py
+++ iSeller/apps/registro/views.py
@@ -12,37 +12,43 @@ from apps.cliente.views import index, perfilCliente
 
 def registro_view(request):
     if request.method == 'POST':
-        form = RegistroForm(request.POST)
+        form = RegistroForm(request.POST or None)
         if form.is_valid():
             form.save()
-            datos = form.cleaned_data
-            name_us = datos.get('usuario')
+            name_us= form.cleaned_data.get("usuario")
             user = Persona.objects.filter(usuario=name_us)
             request.session['id_user'] = user.values()[0]['idPersona']
             request.session['nombres'] = user.values()[0]['nombres']
             request.session['apellidos'] = user.values()[0]['apellidos']
             request.session['isLogin']=True
             form.save()
-        return redirect('/')
+            return redirect('/')
     else:
         form = RegistroForm()
     return render(request,'registro/index.html', {'form':form } )
 
+#permite el logeo por nombre de usuario o correo
+def login_corre_or_username(nameus, clave):
+    user = Persona.objects.filter(usuario=nameus,password=clave)
+    if user.exists():  
+        return user
+    else:
+        user = Persona.objects.filter(email=nameus,password=clave)
+        return user
 
 def login_view(request):
     if request.method == 'POST':
-        form_login=LoginForm(request.POST)
+        form_login=LoginForm(request.POST or None)
         if form_login.is_valid():
-            datos = form_login.cleaned_data
-            name_us = datos.get("username")
-            password_us = datos.get("password")
-            user = Persona.objects.filter(usuario=name_us,password=password_us)
+            name_us = form_login.cleaned_data.get("username")
+            password_us = form_login.cleaned_data.get("password")
+            user = login_corre_or_username(name_us,password_us)
             if user.exists():    
                 request.session['id_user'] = user.values()[0]['idPersona']
                 request.session['nombres'] = user.values()[0]['nombres']
                 request.session['apellidos'] = user.values()[0]['apellidos']
                 request.session['isLogin']=True
-                return redirect('/cliente/perfil') 
+                
     else:
         form_login = LoginForm()
         return render(request,'registro/login.html',{'form_login':form_login})
