5ae538e4363869bb673685ac3d1f54499788d06d
Lehi Qr <lehi_10@hotmail.com>
2017-12-05 20:11:09 -0500

    Fixed Intermediario Model

--- iSeller/apps/intermediario/views.py
+++ iSeller/apps/intermediario/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-
 from django.http import HttpResponse
-from apps.intermediario.models import Intermediario
+
 # Create your views here.
 
 def index(request):
