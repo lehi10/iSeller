3b3f424d1ed578ac960fe802e9ad1fca5b633dc7
Lena <karengvcs@gmail.com>
2017-12-17 17:37:44 -0500

    Creacion y muestra  de pedidos,editar informacion basica de cliente

--- iSeller/apps/cliente/models.py
+++ iSeller/apps/cliente/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 from apps.registro.models import Persona
+from apps.proveedor.models import Producto
 # Create your models here.
 
 class Cliente(models.Model):
@@ -8,5 +10,16 @@ class Cliente(models.Model):
     persona = models.OneToOneField(Persona,null=False,blank=False, on_delete=models.CASCADE)
     ##Falta id carrito de compras 
 
+#tabla de lista de pedidos de cada cliente
+class Pedidos(models.Model):
+	idPedido = models.AutoField(primary_key=True)
+	idCliente = models.ForeignKey(Cliente)
+	nombre = models.CharField(blank=True, max_length=100)
+	descripcion = models.TextField(null=False, max_length=500)
+	categoria = models.CharField(max_length=10)
+	fecha_pedido = models.DateField()
 
-
+#tabla de lista de deseos
+class Lista_deseos(models.Model):
+	idProducto = models.ForeignKey(Producto,null=False,blank=False)
+	idCliente = models.ForeignKey(Persona,null=False,blank=False)
--- iSeller/apps/cliente/views.py
+++ iSeller/apps/cliente/views.py
@@ -1,9 +1,20 @@
+from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.shortcuts import redirect
 from apps.tienda.views import error404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView
 
 from django.http import HttpResponse
 from apps.registro.models import Persona
+from apps.registro.forms import RegistroForm, RegistroUsuarioForm
+from apps.cliente.models import Cliente, Pedidos
+from apps.cliente.forms import CrearPedidoForm
+from django.contrib import auth
+
+
+from datetime import datetime
 # Create your views here.
 
 def index(request):
@@ -17,11 +28,70 @@ def perfilCliente(request):
 	idUsuario = request.session.get('id_user')
 	usuario = Persona.objects.filter(idUsuario_id=idUsuario)
 	contexto= {'mi_usuario':usuario , 'id_user':idUsuario}
-	return render(request,'cliente/perfil.html',contexto)     
+	return render(request,'cliente/perfil.html',contexto)   
 
+#muestra la interfaz de los pedidos del cliente
+def pedidosCliente(request):	
+	#obtenemos el id de la persona usando el id usuario
+	#IDpersona = Persona.objects.get(idUsuario_id =request.session['id_user'])
+	idUsuario = request.session.get('id_user')
+	usuario = Persona.objects.get(idUsuario_id=idUsuario)
+	IDcliente = Cliente.objects.get(persona_id =usuario.idPersona)
+	lista_pedidos = Pedidos.objects.filter(idCliente_id=IDcliente.idCliente)
+	#obtenemos la fecha actual 
+	if request.method == 'POST':
+		form_pedido = CrearPedidoForm(request.POST or None)
+		if form_pedido.is_valid():
+			#obtenemos lod datos llenados desde el formulario
+			datos_pedido = form_pedido.save(commit=False)
+			#E llenamos los campos que no se llenaron desde formulario
+			datos_pedido.fecha_pedido = datetime.now()
+			#Guardamos el id del cliente actual en el pedido realizado
+			datos_pedido.idCliente_id=IDcliente.idCliente
+			datos_pedido.save()
+	else: 
+		form_pedido = CrearPedidoForm()
+	contexto= {'form_pedido':form_pedido,'lista_pedidos':lista_pedidos,'mi_usuario':usuario , 'id_user':idUsuario}
+	return render(request, 'cliente/pedidos.html', contexto)
 
 
 def carritoCliente(request):
     return render(request, 'cliente/carrito.html')
 
-    
+#Editamos informacion del usuario
+def editarInformacion_view(request, id_user):
+	usuario = Persona.objects.get(idPersona = id_user)
+	print(usuario.nombres)
+	if request.method =="POST":
+		form = RegistroForm(request.POST or None)
+		if form.is_valid():
+			usuario.nombres = form.cleaned_data['nombres']
+			usuario.apellidos = form.cleaned_data['apellidos']
+			usuario.sexo = form.cleaned_data['sexo']
+			usuario.fecha_nac = form.cleaned_data['fecha_nac']
+			usuario.id_doc = form.cleaned_data['id_doc']
+			usuario.telf = form.cleaned_data['telf']
+			usuario.domicilio = form.cleaned_data['domicilio']
+			usuario.save()
+			return redirect('/cliente/perfil')
+		#return render(request,'cliente/perfil.html',{'mi_usuario':usuario})   
+	if request.method == "GET":
+		form_editar = RegistroForm(initial={
+			'nombres': usuario.nombres,
+			'apellidos': usuario.apellidos,
+			'sexo':usuario.sexo,
+			'fecha_nac': usuario.fecha_nac,
+			'id_doc': usuario.id_doc,
+			'telf':usuario.id_doc,
+			'domicilio':usuario.domicilio,
+			})
+		contexto = {'form_editar':form_editar, 'usuario':usuario}
+		return render(request,'cliente/editarInf.html',contexto)
+
+def listaDeseos(request):
+	return render(request,'cliente/lista_deseos.html')
+
+
+
+
+
